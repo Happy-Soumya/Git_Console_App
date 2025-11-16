bb59d1f8ab655904e7ede23007402da124d0f4ed
Happy-Soumya <someonesoumya@gmail.com>
2025-11-16 22:46:30 +0530

    xxxyyyzzz

--- Program.cs
+++ Program.cs
@@ -2,16 +2,16 @@
 
 Employee employee = new ();
 
-employee.Id = 1;
-employee.Name = "Soumya Ranjan Ojha";
-employee.Salary = 89000;
+employee.Id = 10;
+employee.Name = "Soumya Ranjan Das";
+employee.Salary = 9000;
 //Comment Added
 
 Console.WriteLine("Name is "+employee.Name + " having ID : " + employee.Id + " of salary "+ employee.Salary);
 
 Customer customer = new ();
 customer.Id = 1;
-customer.Name = "Happy";
+customer.Name = "Avhisekh gupta";
 
 Console.WriteLine(customer.Name + " having ID : " + customer.Id);
 
